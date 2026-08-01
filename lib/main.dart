a11ed099edc4cb847786399903e0b446350b58b5
smflasher927 <goyangaja257@gmail.com>
2026-08-01 23:54:16 +0700

    Create main.dart for SMFLASHER Nota app
    Initial implementation of the SMFLASHER Nota application with a basic UI.

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
+import 'dart:typed_data';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SMFLASHER Nota',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset('assets/logo_smf.png', height: 30),
+            SizedBox(width: 10),
+            Text('SMFLASHER Nota'),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Aplikasi Nota SMFLASHER\nTinggal tambah fitur', textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
